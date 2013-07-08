65edb5179ea21648c5727561a24d3a9103df737b
Ryan Welton <Ryan.G.Welton@gmail.com>
2013-07-08 14:17:23 -0400

    Building of a .jar + command line parsing

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,7 @@
 import sbt._
 import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object Versions {
   val scala     = "2.10.2"
@@ -7,11 +9,46 @@ object Versions {
 }
 
 object AndroidMasterKeys extends Build {  
-    lazy val masterKeys = Project("manifestParser", file("."), settings = masterKeySettings)
-    lazy val masterKeySettings = Defaults.defaultSettings ++ Seq(
-	    name := "Android Master Keys",
-	    scalaVersion := Versions.scala,
-	    libraryDependencies ++= Seq(
-	    )
-	)
+  val projectName = "AndroidMasterKeys"
+  val ver = "0.1"
+  val mainClassName = "com.apk.service.web.Standalone"
+
+  lazy val masterKeys = Project(projectName, file("."), settings = masterKeySettings)
+  lazy val masterKeySettings = Defaults.defaultSettings ++ assemblySettings ++ Seq(
+    name := projectName,
+    version := ver,
+    scalaVersion := Versions.scala,
+    jarName in assembly := projectName + ".jar",
+    mainClass in assembly := Some(mainClassName),
+    mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+      {
+        case "application.conf" => MergeStrategy.concat
+        case "reference.conf"   => MergeStrategy.concat
+        case "mime.types"       => MergeStrategy.filterDistinctLines
+        case PathList("org", "hamcrest", _ @ _*) => MergeStrategy.first
+        case PathList("com", "google", "common", _ @ _*) => MergeStrategy.first
+        case PathList("org", "xmlpull", _ @ _*) => MergeStrategy.first
+        case PathList(ps @ _*) if ps.last.toLowerCase.startsWith("notice") ||
+                                  ps.last.toLowerCase == "license" || 
+                                  ps.last.toLowerCase == "license.txt" || 
+                                  ps.last.toLowerCase == "asm-license.txt" ||
+                                  ps.last.endsWith(".html") => MergeStrategy.rename 
+        case PathList("META-INF", xs @ _*) =>
+          (xs map {_.toLowerCase}) match {
+            case ("manifest.mf" :: Nil) | ("index.list" :: Nil) | ("dependencies" :: Nil) =>
+              MergeStrategy.discard
+            case ps @ (x :: xs) if ps.last.endsWith(".sf") || ps.last.endsWith(".dsa")  =>
+              MergeStrategy.discard
+            case "services" :: xs =>
+              MergeStrategy.filterDistinctLines
+            case _ => MergeStrategy.deduplicate
+          }
+        case _ => MergeStrategy.deduplicate
+      }
+    },
+    libraryDependencies ++= Seq("com.github.scopt" %% "scopt" % "2.1.0",
+                               "org.scalatest" %% "scalatest" % Versions.scalatest % "test"),
+    mainClass in Compile := Some(mainClassName),
+    aggregate in run := false
+  )
 }
